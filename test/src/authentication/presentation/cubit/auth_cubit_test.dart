ab9d48cd5b27c9c089b1e54993d372b6167d5378
Prince Tomar <tomarprince443@gmail.com>
2025-02-15 11:14:51 +0530

    authentication creating user and getting user cubits tests done

--- test/src/authentication/presentation/cubit/auth_cubit_test.dart
+++ test/src/authentication/presentation/cubit/auth_cubit_test.dart
@@ -1,5 +1,9 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:tddtut/core/errors/failures.dart';
+import 'package:tddtut/src/authentication/data/models/user_model.dart';
 import 'package:tddtut/src/authentication/domain/usecases/create_user.dart';
 import 'package:tddtut/src/authentication/domain/usecases/get_users.dart';
 import 'package:tddtut/src/authentication/presentation/cubit/auth_cubit.dart';
@@ -13,6 +17,10 @@ void main() {
   late CreateUserUsecase createUser;
   late AuthCubit cubit;
 
+  const tCreateUserParams = CreateUserParams.empty();
+  const tApiFailure = ApiFailure(message: 'API Failure', statusCode: 303);
+  const tUsers = [UserModel.empty()];
+
   setUp(() {
     getUsers = MockGetUsers();
     createUser = MockCreateUsers();
@@ -20,5 +28,97 @@ void main() {
       createUserUsecase: createUser,
       getUsersUsecase: getUsers,
     );
+    registerFallbackValue(tCreateUserParams);
+  });
+
+  tearDown(() => cubit.close());
+
+  test('initial state should be [AuthenticationInitial]', () async {
+    expect(cubit.state, AuthInitial());
+  });
+
+  group('Creater User', () {
+    blocTest<AuthCubit, AuthState>(
+      'emits [CreatingUser, UserCreated] when user is created successfully.',
+      build: () {
+        when(() => createUser(tCreateUserParams)).thenAnswer(
+          (_) async => const Right(null),
+        );
+        return cubit;
+      },
+      act: (cubit) => cubit.createUser(
+        createdAt: tCreateUserParams.createdAt,
+        name: tCreateUserParams.name,
+        avatar: tCreateUserParams.avatar,
+      ),
+      expect: () => <AuthState>[
+        UserCreating(),
+        UserCreated(),
+      ],
+      verify: (cubit) {
+        verify(() => createUser(tCreateUserParams)).called(1);
+        verifyNoMoreInteractions(createUser);
+      },
+    );
+  });
+
+  blocTest<AuthCubit, AuthState>(
+    'emits [CreatingUser, AuthenticationError] when user is created successfully.',
+    build: () {
+      when(() => createUser(tCreateUserParams)).thenAnswer(
+        (_) async => const Left(tApiFailure),
+      );
+      return cubit;
+    },
+    act: (cubit) => cubit.createUser(
+      createdAt: tCreateUserParams.createdAt,
+      name: tCreateUserParams.name,
+      avatar: tCreateUserParams.avatar,
+    ),
+    expect: () => <AuthState>[
+      UserCreating(),
+      AuthError(errorMessage: tApiFailure.errorMessage),
+    ],
+    verify: (cubit) {
+      verify(() => createUser(tCreateUserParams)).called(1);
+      verifyNoMoreInteractions(createUser);
+    },
+  );
+
+  group('getting Users', () {
+    blocTest<AuthCubit, AuthState>(
+        'emits [GettingUsers, UsersFetched] when called fetchUsers function',
+        build: () {
+          when(() => getUsers()).thenAnswer((_) async => const Right(tUsers));
+          return cubit;
+        },
+        act: (cubit) => cubit.fetchUsers(),
+        expect: () => <AuthState>[
+              GettingUsers(),
+              const UsersFetched(
+                users: tUsers,
+              ),
+            ],
+        verify: (_) {
+          verify(() => getUsers()).called(1);
+          verifyNoMoreInteractions(getUsers);
+        });
+
+    blocTest<AuthCubit, AuthState>(
+        'emits [GettingUsers, AuthError] when failed fetchUsers function',
+        build: () {
+          when(() => getUsers())
+              .thenAnswer((_) async => const Left(tApiFailure));
+          return cubit;
+        },
+        act: (cubit) => cubit.fetchUsers(),
+        expect: () => <AuthState>[
+              GettingUsers(),
+              AuthError(errorMessage: tApiFailure.errorMessage),
+            ],
+        verify: (_) {
+          verify(() => getUsers()).called(1);
+          verifyNoMoreInteractions(getUsers);
+        });
   });
 }
